3544460d16114d2e580f7703677b7306ff17714b
sadumina <it23394124@my.sliit.lk>
2026-01-28 11:44:24 +0530

    The reason for checking ui

--- tests/ui-test.spec.js
+++ tests/ui-test.spec.js
@@ -8,3 +8,5 @@ test('UI_Test_001: Input feild accept text', async ({ page }) => {
     await singlishInput.fill('Hello yaluwane');
     await expect(singlishInput).toHaveValue('Hello yaluwane');     
 }  );
+
+//The reason for the above test case is to verify that the input field on the Swift Translator website correctly accepts and displays user-entered text in Singlish.
